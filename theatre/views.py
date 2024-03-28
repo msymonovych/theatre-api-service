12a50a5450d5745ed0d2bd83d5e37ed82b29e1ba
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-28 19:04:49 +0200

    Add perform_create method to ReservationViewSet

--- theatre/views.py
+++ theatre/views.py
@@ -116,3 +116,6 @@ class ReservationViewSet(viewsets.ModelViewSet):
             return ReservationListSerializer
 
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
