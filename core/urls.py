7d9a2ccee68899a00e3538bc38f6c747b3a4f757
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-28 12:50:18 +0200

    Add endpoints for documentation

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,26 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/theatre/", include("theatre.urls", namespace="theatre")),
-    path("api/users/", include("user.urls", namespace="user"))
+    path("api/users/", include("user.urls", namespace="user")),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/v1/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
+    path(
+        "api/v1/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc"
+    )
 ]
