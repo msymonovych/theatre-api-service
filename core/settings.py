2ee2b16b66d91952c7cd6dea2a76f61943c57d8e
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-28 14:40:08 +0200

    Add JWT authentication to settings

--- core/settings.py
+++ core/settings.py
@@ -128,6 +128,9 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
