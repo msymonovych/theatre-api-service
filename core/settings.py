bf79c71154771e5ea062523ed66a8b5a32d8d2ac
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-31 18:29:58 +0300

    Add simple_jwt settings

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -139,4 +139,10 @@ SPECTACULAR_SETTINGS = {
     "DESCRIPTION": "Booking a ticket for your favourite play",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEME": False,
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
 }
